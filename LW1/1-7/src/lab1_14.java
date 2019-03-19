c19b775a0019c358c2f900ea7a11b7f86c489eb9
Artem <artem199929@gmail.com>
2019-03-19 16:44:18 +0000

    exchange files in folders, nothing else

--- LW1/1-7/src/lab1_14.java
+++ LW1/1-7/src/lab1_14.java
@@ -9,7 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class lab1_30 {
+public class lab1_14 {
 	public static void addComponentsToPane(Container pane) {
 		pane.setLayout(new GridBagLayout());
 		GridBagConstraints constraints = new GridBagConstraints();
@@ -18,53 +18,50 @@ public class lab1_30 {
 
 		constraints.gridx = 0;
 		constraints.gridy = 0;
-		pane.add(new JLabel("Делимое"), constraints);
+		pane.add(new JLabel("год"), constraints);
 
 		constraints.gridx = 1;
 		constraints.gridy = 0;
-		constraints.gridwidth = 1;
-		JTextField moeTextField = new JTextField();
-		pane.add(moeTextField, constraints);
-
-		constraints.gridx = 0;
-		constraints.gridy = 1;
-		pane.add(new JLabel("Делитель"), constraints);
-
-		constraints.gridx = 1;
-		constraints.gridy = 1;
-		constraints.gridwidth = 1;
+		constraints.gridwidth = 3;
 		JTextField telTextField = new JTextField();
 		pane.add(telTextField, constraints);
+		
+		constraints.gridx = 5;
+		constraints.gridy = 0;
+		JLabel kolLabel = new JLabel("Количество дней");
+		pane.add(kolLabel, constraints);
+
+		constraints.gridx = 5;
+		constraints.gridy = 1;
+		JLabel visLabel = new JLabel("Високосный");
+		pane.add(visLabel, constraints);
 
 		constraints.gridx = 0;
-		constraints.gridy = 2;
-		JLabel chastLabel = new JLabel("Частное");
-		pane.add(chastLabel, constraints);
-
-		constraints.gridx = 3;
-		constraints.gridy = 2;
-		JLabel ostLabel = new JLabel("Остаток");
-		pane.add(ostLabel, constraints);
-
-		constraints.gridx = 3;
 		constraints.gridy = 1;
-		JButton podelitButton = new JButton("Поделить");
+		constraints.gridwidth = 5;
+		JButton podelitButton = new JButton("Проверить");
 		podelitButton.setPreferredSize(new Dimension(15, 20));
 		pane.add(podelitButton, constraints);
 
 		podelitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Integer moe = Integer.parseInt(moeTextField.getText());
-				Integer tel = Integer.parseInt(telTextField.getText());
-
-				chastLabel.setText("Частное: " + moe / tel);
-				ostLabel.setText("Остаток: " + moe % tel);
+				Integer year = Integer.parseInt(telTextField.getText());
+				String condition = "Нет";
+				int kolvo = 365;
+				
+				if((year%4)==0) {
+					condition = "Да";
+					kolvo=366;
+				}
+				
+				visLabel.setText("Високосный " + condition);
+				kolLabel.setText("Количество дней " + kolvo);
 			}
 		});
 	}
 
 	private static void createAndShowGUI() {
-		JFrame frame = new JFrame("Division");
+		JFrame frame = new JFrame("Years");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(330, 90);
 		frame.setResizable(false);
