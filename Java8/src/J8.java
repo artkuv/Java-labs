f8700160b95757c1040a72771422139a8954a7ff
Artem Kulchitsky <41903780+artkuv@users.noreply.github.com>
2019-05-20 12:52:20 +0300

    final Java8 task

--- Java8/src/J8.java
+++ Java8/src/J8.java
@@ -7,19 +7,21 @@ import java.util.stream.Collectors;
 public class J8{
 	
 	public static void main(String[] args) {
-		ArrayList<Integer>	testValuesNull = new ArrayList();
+		ArrayList<Integer> testValuesNull = new ArrayList();
 		testValuesNull.add(0, null);
 		testValuesNull.add(1,-1);
 		testValuesNull.add(2,-70);
 		testValuesNull.add(3,-2);
 		testValuesNull.add(4,-2);
 		
+		//using Optional and stream
 		Optional<Integer> maxValueNotNull = testValuesNull.stream().filter((p) -> p != null).max(Integer::compareTo);
 		System.out.println("maxValueNotNull="+maxValueNotNull);
 		
 		List<Integer> only2 = testValuesNull.stream().filter((p)-> p!=null && p == -2).collect(Collectors.toList());
 		System.out.println("only2="+only2);
 		
+		//sort with using lambda function
 		testValuesNull.remove(0);
 		System.out.println("BEFORE:\ntestValuesNull="+testValuesNull);
 		
